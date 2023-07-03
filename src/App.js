4ed0a634fadfe48fabf53f77acdaab3aeec0e849
Azwar <Azwar@tanbits.com>
2023-07-03 21:38:17 +0500

    LogoutButton

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
 import { LoginButton } from "./Login";
-import { LogoutButton } from "./Logout";
 import { Profile } from "./Profile";
 import logo from "./logo.svg";
 import "./App.css";
