d785c509d7e6b81d831b9c8072f1a31bb0d18d61
Hammad <hammad@tanbits.com>
2023-07-11 11:32:10 +0500

    Revert "LogoutButton"
    This reverts commit 4ed0a634fadfe48fabf53f77acdaab3aeec0e849.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
 import { LoginButton } from "./Login";
+import { LogoutButton } from "./Logout";
 import { Profile } from "./Profile";
 import logo from "./logo.svg";
 import "./App.css";
@@ -15,6 +16,7 @@ function App() {
         {isAuthenticated ? (
           <>
             <Profile />
+              <LogoutButton />
           </>
         ) : (
           <LoginButton />
